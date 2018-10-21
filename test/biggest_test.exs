535542843d12d99336be2535588d602ba229224b
Christopher Eyre <chriseyre2000@gmail.com>
2018-10-21 10:32:14 +0100

    Fixed up the basic tests

--- test/biggest_test.exs
+++ test/biggest_test.exs
@@ -3,6 +3,7 @@ defmodule BiggestTest do
   use PropCheck 
 
   property "check biggest works" do
+    # I am experimenting with the Elixir unrolled version of the functions.
     forall list <- integer() |> list() |> non_empty() do
       #State an invariant
       biggest(list) === list |> Enum.sort |> List.last  
@@ -10,7 +11,21 @@ defmodule BiggestTest do
   end
 
   #Method to be tested
-  defp biggest([h|_t]), do: h
+  defp biggest([h|t]) do
+     biggest(t, h)
+  end
+
+  defp biggest([], maxima) do
+    maxima
+  end
+
+  defp biggest([h|t], maxima) when h >= maxima do
+    biggest(t, h)
+  end
+  
+  defp biggest([h|t], maxima) when h < maxima do
+    biggest(t, maxima)
+  end
 
 
 end
